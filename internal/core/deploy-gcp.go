0ab65cb3fca63dc5f9b84d9fb0ddd0918bb1c5e9
aidocons <consultant-butlyai@aidodev.com>
2025-05-12 13:58:30 +0100

    config init and plugin install

---
+++ internal/core/deploy-gcp.go
@@ -0,0 +1,89 @@
+package core
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/iam/apiv1/iampb"
+	"cloud.google.com/go/run/apiv2"
+	runpb "cloud.google.com/go/run/apiv2/runpb"
+)
+
+type PigenCoreGCP struct {
+	ProjectID string
+	Region    string
+}
+
+func (g *PigenCoreGCP) DeployPigenCore() (string, error) {
+	ctx := context.Background()
+	containerPort := &runpb.ContainerPort{
+		ContainerPort:5000,
+	}
+	googleCloudRunV2Container := &runpb.Container{
+		Image: "fedimersni/pigen-core:latest",
+		Name: "pigen-core",
+		Ports: []*runpb.ContainerPort{containerPort},
+	}
+
+	revisionTemplate := &runpb.RevisionTemplate{
+		Containers: []*runpb.Container{googleCloudRunV2Container},
+	}
+	service := &runpb.Service{
+		Template: revisionTemplate,
+		Ingress: runpb.IngressTraffic_INGRESS_TRAFFIC_ALL,
+	}
+
+	createServiceRequest := &runpb.CreateServiceRequest{
+		Parent: "projects/"+g.ProjectID+"/locations/" + g.Region,
+		Service: service,
+		ServiceId:"pigen-core",
+	}
+	client, err := run.NewServicesClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("error waiting for Cloud Run service creation: %v", err)
+	}
+	op, err := client.CreateService(ctx, createServiceRequest)
+	if err != nil {
+		return "", fmt.Errorf("failed to create Cloud Run service: %v", err)
+	}
+
+	resp, err := op.Wait(ctx)
+	if err != nil {
+		return "", fmt.Errorf("cloud run creator failed to wait for service creation: %v", err)
+	}
+	// Get the service name
+	serviceName := resp.Name
+
+	// Set up IAM policy to allow unauthenticated invocations
+	iamClient, err := run.NewServicesClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("error creating IAM client: %v", err)
+	}
+
+	// First, get the current IAM policy
+	getIamPolicyRequest := &iampb.GetIamPolicyRequest{
+		Resource: serviceName,
+	}
+	policy, err := iamClient.GetIamPolicy(ctx, getIamPolicyRequest)
+	if err != nil {
+		return "", fmt.Errorf("error getting IAM policy: %v", err)
+	}
+
+	// Add the binding for allUsers
+	policy.Bindings = append(policy.Bindings, &iampb.Binding{
+		Role:    "roles/run.invoker",
+		Members: []string{"allUsers"},
+	})
+
+	// Set the updated policy
+	setIamPolicyRequest := &iampb.SetIamPolicyRequest{
+		Resource: serviceName,
+		Policy:   policy,
+	}
+	_, err = iamClient.SetIamPolicy(ctx, setIamPolicyRequest)
+	if err != nil {
+		return "", fmt.Errorf("error setting IAM policy: %v", err)
+	}
+
+	return resp.Uri, nil
+}
---
+++ internal/plugin/install.go
@@ -0,0 +1,49 @@
+package plugin
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	shared "github.com/pigen-dev/shared"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
+)
+
+type PluginFile struct {
+	Plugins []shared.PluginStruct `yaml:"plugins"`
+}
+
+func PluginInstall(filePath string) error {
+	var pluginFile PluginFile
+	fmt.Println("Installing plugins from file:", filePath)
+	// Read the file content
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to read plugin file: %w", err)
+	}
+	err = yaml.Unmarshal(fileContent, &pluginFile)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal plugin file: %w", err)
+	}
+	pigenCoreEndpoint := fmt.Sprintf("%s/api/v1/plugin/setup_plugin", viper.GetString("config.pigen_core.endpoint"))
+
+	for _, plugin := range pluginFile.Plugins {
+		fmt.Println("Installing plugin:", plugin.Plugin.Label)
+		jsonData, err := json.MarshalIndent(plugin, "", "  ")
+		if err != nil {
+			return fmt.Errorf("failed to marshal plugin data into json: %w", err)
+		}
+		resp, err := http.Post(pigenCoreEndpoint, "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			return fmt.Errorf("failed to send request: %w", err)
+		}
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+		fmt.Println("Response:", string(body))
+	}
+	return nil
+}
